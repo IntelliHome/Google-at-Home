b0a8ba1c58bc96e0a9777ae420c154f23198e083
mudler <mudler@dark-lab.net>
2013-07-23 15:46:56 +0200

    added time elapsed

--- lib/IH/GSynth.pm
+++ lib/IH/GSynth.pm
@@ -1,5 +1,6 @@
 package IH::GSynth;
 use Moo;
+use Time::HiRes qw(usleep ualarm gettimeofday tv_interval);
 require IH::Interfaces::Terminal;
 require  LWP::UserAgent;
 
@@ -7,13 +8,17 @@ has 'File' => (is=>"rw");
 has 'Output' => (is=>"rw",default=> sub{ return new IH::Interfaces::Terminal});
 has 'GoogleURL' => (is=>"rw",default=>"https://www.google.com/speech-api/v1/recognize?xjerr=1&maxresults=10&client=speech2text&lang=" );
 has 'Language' => (is=>"rw",default=> "it");
-
+has 'hypotheses' => (is=>"rw");
+has 'Time' => (is=>"rw");
 sub synth(){
 	my $self=shift;
+	    my $request_arrival_time = [gettimeofday];
+
 	my $url = $self->GoogleURL. $self->Language;
 	my @hypotheses = ();
-	my $audio      = '';
-	$self->Output()->info("Synthesys of ".$Self->File ." delegated to google (do you know a better machine learning as googlebrain?)");
+	my $audio;
+	my $result;
+	$self->Output()->info("Synthesys of ".$self->File ." delegated to google (do you know a better machine learning as googlebrain?)");
  	open( FILE, "<" . $self->File );
 	while (<FILE>) {
 		$audio .= $_;
@@ -31,8 +36,10 @@ sub synth(){
 	while (  $result =~ m/\"utterance\"\:\"(.*?)\"/g ) {
 		push( @hypotheses, $1 );
 	}
-	unlink($Self->File);
-	
+	unlink($self->File);
+	$self->hypotheses(\@hypotheses);
+	    my $elapsed = tv_interval( $request_arrival_time, [gettimeofday] );
+	$self->Time($elapsed);
 	return 0 if @hypotheses <= 0;
 	return @hypotheses;
 }
