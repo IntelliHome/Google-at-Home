1b932ddf1a2598f6540228e16dd9bb4ae887b6be
mudler <mudler@dark-lab.net>
2013-07-24 16:08:34 +0200

    fixed output directory

--- lib/IH/Recorder/Sox.pm
+++ lib/IH/Recorder/Sox.pm
@@ -5,24 +5,25 @@ with("IH::Workers::Process");
 has 'command'             => ( is => "rw" );
 has 'Rate'                => ( is => "rw", default => "16000" );
 has 'HW'                  => ( is => "rw", default => "0,0" );
-has 'Output'              => ( is => "rw", default => "out.flac" );
+has 'Output'              => ( is => "rw", default => "ih.flac" );
 has 'beginEnable'         => ( is => "rw", default => "1" );
 has 'beginSoundDuration'  => ( is => "rw", default => "0.5" );
 has 'beginThreshold'      => ( is => "rw", default => '1%' );
 has 'finishEnable'        => ( is => "rw", default => "1" );
 has 'finishSoundDuration' => ( is => "rw", default => "2.0" );
 has 'finishThreshold'     => ( is => "rw", default => '2%' );
-has 'outputDir'           => ( is => "rw", default => "/tmp/sox" );
+has 'outputDir'           => ( is => "rw", default => "/tmp/sox/" );
 
 sub _generateOutputCommand() {
     my $self = shift;
+   mkdir($self->outputDir) if (!-d $self->outputDir);
 
     $self->command(
 
         "sox -b 32 -t alsa hw:"
             . $self->HW() . " -r "
             . $self->Rate() . " "
-            . $self->Output()
+            . $self->outputDir. $self->Output()
             . " silence "
             . $self->beginEnable() . " "
             . $self->beginSoundDuration . " "
