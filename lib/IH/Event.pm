ee1295da18de72f66503bf6e963e03d2fb9abe92
mudler <mudler@dark-lab.net>
2013-07-26 21:43:23 +0200

    adjustments

--- lib/IH/Event.pm
+++ lib/IH/Event.pm
@@ -1,11 +1,13 @@
 package IH::Event;
 use IH::Interfaces::Terminal;
 use IH::GSynth;
+use IH::Connector;
 use Data::Dumper;
 use Moo;
 has 'LastModified' =>(is=>"rw");
 has 'Output'       =>(is=>"rw",default=>sub{return new IH::Interfaces::Terminal});
 has 'GSynth'       =>(is=>"rw",default=>sub{return new IH::GSynth});
+has 'Connector'    => (is=>"rw",default=> sub{return new IH::Connector});
 sub events() {
     my $self = shift;
     foreach my $event (@_) {
@@ -29,22 +31,23 @@ sub process_deleted_file {
     $self->Output->info("\^$File\^ deleted");
 }
 
-sub process_created_file {
+sub process_created_file { 
     my $self = shift;
     my $File = shift;
     $self->Output->info("\^$File\^ created");
     if($self->LastModified){
-            $self->Output->info("processing \^".$self->LastModified."\^");
+        $self->Output->info("processing \^".$self->LastModified."\^");
+        $self->Connector->send_file($self->LastModified);
 
-        my $Synth=$self->GSynth;
-        $Synth->File($self->LastModified);
-        $Synth->synth();
-        my @hypotheses=@{$Synth->hypotheses()};
-        if(@hypotheses <= 0){
-            $self->Output->info("No result from google elapsed ".$Synth->Time."s");
-        } else {
-            $self->Output->info("Google result : ".join("\t",  @hypotheses)." ".$Synth->Time."s")  ;
-        }
+        # my $Synth=$self->GSynth;
+        # $Synth->File($self->LastModified);
+        # $Synth->synth();
+        # my @hypotheses=@{$Synth->hypotheses()};
+        # if(@hypotheses <= 0){
+        #     $self->Output->info("No result from google elapsed ".$Synth->Time."s");
+        # } else {
+        #     $self->Output->info("Google result : ".join("\t",  @hypotheses)." ".$Synth->Time."s")  ;
+        # }
         $self->LastModified(0);
     }
 
