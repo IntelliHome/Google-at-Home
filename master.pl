2eb2f338098efd32cdaa08aede3bdb6d25d013a7
mudler <mudler@dark-lab.net>
2013-07-28 14:11:24 +0200

    testing broadcast

--- master.pl
+++ master.pl
@@ -13,12 +13,12 @@ my $IHOutput = new IH::Interfaces::Terminal;
 $IHOutput->info("IntelliHome : Node master started");
 $IHOutput->info("Bringing up sockets (not secured, i assume you have vpn on your network)");
 
-
+my $Config= new IH::Config( Dirs=> [ './config']); #specify where yaml file are
+$Config->read(); # Read and load yaml configuration
 
 my $Delegate=new IH::Delegate;
-my $Connector= new IH::Connector(Host=>"localhost");
-
-
-
+my $Connector= new IH::Connector(Config=>$Config,Host=>"localhost",Port=>23457);
 $Connector->Worker($Delegate);
-$Connector->listen();
+#$Connector->listen();
+
+$Connector->broadcastMessage("node","test");
