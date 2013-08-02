8ccba1d4ea961d7b9cc782392ecf06b2d0994166
mudler <mudler@dark-lab.net>
2013-08-02 22:33:17 +0200

    -a

--- lib/IntelliHomeNode.pm
+++ lib/IntelliHomeNode.pm
@@ -6,4 +6,6 @@ require IH::Monitor;
 require IH::Config;
 require Proc::Daemon;
 require AnyEvent;
+require IH::Node;
+
 1;
--- lib/IntelliHomeNodeMaster.pm
+++ lib/IntelliHomeNodeMaster.pm
@@ -4,4 +4,5 @@ require IH::Connector;
 require IH::Config;
 require IH::Delegate;
 require IH::DB;
+require IH::Node;
 1;
