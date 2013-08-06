d8dc1ba610801879ef9bff063aa05681d35b4621
mudler <mudler@dark-lab.net>
2013-08-06 12:28:29 +0200

    this solution is readable and nice

--- lib/IH/Deployer/Base.pm
+++ lib/IH/Deployer/Base.pm
@@ -1,4 +1,8 @@
 package IH::Deployer::Base;
 use Moo;
+use IH::Interface::Terminal;
 
 has 'Node' => (is=>"rw");
+has 'Output' => (is=>"rw" , default=> sub{return new IH::Interface::Terminal});
+
+1;
--- lib/IH/Deployer/Debian.pm
+++ lib/IH/Deployer/Debian.pm
@@ -1,8 +1,35 @@
 package IH::Deployer::Debian;
 use Moo;
+use Net::SSH::Any;
+
 extends 'IH::Deplyer::Base';
 
-sub deploy{
-	print "test\n"; #all to write here :)
+sub deploy(){
+	my $self=shift;
+
+my $ssh = Net::SSH::Any->new($self->Node, user => $self->Node->Username, password => $self->Node->Password);
+ 
+#my @out = $ssh->capture(cat => "/etc/passwd");
+#my ($out, $err) = $ssh->capture2("ls -l /");
+$ssh->system("foo");
+
+if(!$ssh->system('apt-get install --force-yes libparallel-iterator-perl libmodule-depends-perl cpanminus git libfile-homedir-perl libmoo-perl libmoose-perl liblog-any-adapter-perl liblog-any-perl build-essential libdpkg-perl libapt-pkg-perl')){
+	$self->Output->info("git, cpanminus and few deps are correctly installed");
+	$self->Output->info("Cloning from repository");
+
+
+}
+ 
+#my $sftp = $ssh->sftp; # returns Net::SFTP::Foreign object
+#$sftp->put($local_path, $remote_path);
+
+
 }
 1;
+
+__END__
+
+git clone https://github.com/mudler/IntelliHome.git
+cd IntelliHome/
+cpanm Debian::Apt::PM
+perl -MDebian::Apt::PM -MModule::Depends -le '$apm=Debian::Apt::PM->new();$md=Module::Depends->new->dist_dir(".")->find_modules; %r=(%{$md->requires},%{$md->build_requires}); while (($m, $v) = each %r) { $f=$apm->find($m, $v); print $f->{"min"}->{"package"} if $f->{"min"}  }' | sort | uniq | xargs echo apt-get install
