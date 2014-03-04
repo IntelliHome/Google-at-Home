e3b5661d58227c5fc9605fbc0c24fe1d37454657
mudler <mudler@dark-lab.net>
2014-03-04 14:11:26 +0100

    re-enabling output of synth for debugging

--- lib/IH/Workers/RemoteSynth.pm
+++ lib/IH/Workers/RemoteSynth.pm
@@ -11,16 +11,16 @@ has 'Output' =>
     ( is => "rw", default => sub { return IH::Interfaces::Voice->new } );
 has 'Parser' => ( is => "rw", default => sub { return IH::Parser->new } );
 
-sub process() {
+sub process {
     my $self = shift;
     my $fh   = shift;    ## IO::Socket
     my $audio;
     my $host = $fh->peerhost();
 
-        my $Client = IH::Node->new( Config => $self->Config );
+    my $Client = IH::Node->new( Config => $self->Config );
     $Client->selectFromHost($host);
-  $Client->selectFromType("node");
-$self->Output->Node($Client);
+    $Client->selectFromType("node");
+    $self->Output->Node($Client);
     while (<$fh>) {
         $audio .= $_;
     }
@@ -29,18 +29,18 @@ $self->Output->Node($Client);
     my @hypotheses = @{ $self->GSynth->hypotheses() };
     if ( @hypotheses <= 0 ) {
 
-        #$self->Output->info( "Penso di non aver ben capito" ); #### XXX: Doesn't know what to say
+#$self->Output->info( "Penso di non aver ben capito" ); #### XXX: Doesn't know what to say
 
     }
     else {
 
-
-         $self->Parser->Node($Client);
-         $self->Parser->parse(@hypotheses);
+        $self->Parser->Node($Client);
+        $self->Parser->parse(@hypotheses);
 
         #$self->Output->info( $hypotheses[0] );
 
-#     $self->Output->info("Google result for ".$host.": ".join("\t",  @hypotheses)." ".$self->GSynth->Time."s")  ;
+     $self->Output->info("Google result for ".$host.": ".join("\t",  @hypotheses)." ".$self->GSynth->Time."s");
+     #Let's visualize the hypotesis for now
     }
 }
 
