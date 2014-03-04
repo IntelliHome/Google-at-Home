029ac10f574fea7cf99886a7bafb47c86b36d54f
mudler <mudler@dark-lab.net>
2014-03-04 13:55:10 +0100

    cleaning code

--- lib/IH/Deployer/Debian.pm
+++ lib/IH/Deployer/Debian.pm
@@ -4,11 +4,11 @@ use Net::SSH::Any;
 
 extends 'IH::Deployer::Base';
 
-sub deploy(){
+sub deploy{
     return undef;
 }
 
-sub deploy_t() {
+sub deploy_t {
     my $self = shift;
 
     my $ssh = Net::SSH::Any->new(
