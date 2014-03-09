8c3bcd7bddfa523d7c9ddc30ce8bce1fc4060197
mudler <mudler@dark-lab.net>
2014-03-09 21:10:57 +0100

    Workers base class

---
+++ lib/IH/Workers/Base.pm
@@ -0,0 +1,43 @@
+package IH::Workers::Base;
+use Moose;
+
+use Data::Dumper;
+
+=head1 NAME
+
+IH::Workers::Base - Processes the voice hypothesis thru a defined parser
+
+=head1 DESCRIPTION
+
+This Object implement process() that is called by the master node to parse and process the given command
+
+
+=head1 ARGUMENTS
+
+=over
+=item Config()
+Get/Set the IH::Config object that has all the loaded configurations (required)
+=item Output()
+Get/Set the answer output interface (default to IH::Interfaces::Voice)
+
+
+=head1 IMPLEMENTS
+=over
+=item process()
+Process the worker request
+=back
+=cut
+
+has 'Config' => ( is => "rw" );
+has 'Output' => (
+    is      => "rw",
+    default => sub {
+        return IH::Interfaces::Voice->new 
+    }
+);
+
+sub process () {
+
+}
+
+1;
