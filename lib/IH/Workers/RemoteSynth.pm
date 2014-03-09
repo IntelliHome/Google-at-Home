91ebdd00a3a0b969c4d79bf37817a74e759e217c
mudler <mudler@dark-lab.net>
2014-03-09 21:11:16 +0100

    Database Parser backend switchable

--- lib/IH/Workers/RemoteSynth.pm
+++ lib/IH/Workers/RemoteSynth.pm
@@ -1,15 +1,58 @@
 package IH::Workers::RemoteSynth;
-use Moo;
+use Moose;
+use IH::Workers::Base;
+extends 'IH::Workers::Base';
 use IH::Google::Synth;
 use IH::Interfaces::Voice;
-use IH::Parser;
 use Data::Dumper;
-has 'GSynth' =>
-    ( is => "rw", default => sub { return IH::Google::Synth->new } );
-has 'Config' => ( is => "rw" );
-has 'Output' =>
-    ( is => "rw", default => sub { return IH::Interfaces::Voice->new } );
-has 'Parser' => ( is => "rw", default => sub { return IH::Parser->new } );
+use Module::Load;
+
+=head1 NAME
+
+IH::Workers::RemoteSynth - Processes the voice hypothesis thru a defined parser
+
+=head1 DESCRIPTION
+
+This Object implement process() that is called by the master node to parse and process the given command
+
+
+=head1 ARGUMENTS 
+
+RemoteSynth implements the IH::Workers::Base arguments and implement the follow one
+
+=over
+=item GSynth() 
+Get/Set the used Synthetizer (defaults to IH::Google::Synth)
+=back
+=item Parser()
+Get/Set the parser used to process the command (default loaded from Config)
+=back
+
+=head1 FUNCTIONS
+=over
+=item process()
+Process the request with the parser specified in the config file
+=back
+
+
+=cut
+
+has 'GSynth' => (
+    is      => "rw",
+    default => sub {
+        return IH::Google::Synth->new;
+    }
+);
+has 'Parser' => ( is => "rw", );
+
+sub BUILD {
+    my $self = shift;
+    my $Parser
+        = 'IH::Parser::'
+        . $self->Config->DBConfiguration->{'database_backend'};
+    load $Parser;
+    $self->Parser( $Parser->new );
+}
 
 sub process {
     my $self = shift;
@@ -28,9 +71,8 @@ sub process {
     $self->GSynth->audiosynth($audio);
     my @hypotheses = @{ $self->GSynth->hypotheses() };
     if ( @hypotheses <= 0 ) {
-
-#$self->Output->info( "Penso di non aver ben capito" ); #### XXX: Doesn't know what to say
-
+        #### XXX: Doesn't know what to say
+        #$self->Output->info( "Penso di non aver ben capito" );
     }
     else {
 
