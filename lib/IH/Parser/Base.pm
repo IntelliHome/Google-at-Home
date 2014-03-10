9bcb2377593b60d9a0eede355bc41269a3df5a3f
mudler <mudler@dark-lab.net>
2014-03-10 20:36:54 +0100

    Plugin based parser

--- lib/IH/Parser/Base.pm
+++ lib/IH/Parser/Base.pm
@@ -1,5 +1,7 @@
 package IH::Parser::Base;
-use Moo;
+use Moose;
+has 'Config' => ( is => "rw" );
+has 'Plugins' => ( is => "rw", default => sub{{}} );
 
 sub detectTasks() {
 
@@ -12,4 +14,28 @@ sub detectTriggers() {
 sub parse() {
 
 }
+
+sub prepare() {
+
+}
+
+sub run_plugin() {
+    my $self = shift;
+    my $name = shift;
+    my @args = @_;
+    my $Plugin;
+    if ( exists $self->Plugins->{$name} ) {
+        $Plugin = $self->Plugins->{$name};
+    }
+    else {
+        $Plugin = 'IH::Plugin::' . $name;
+        load $Plugin;
+        $self->Plugins->{$name}
+            = $Plugin->new( Config => $self->Config, Parser => $self );
+        $self->Plugins->{$name}->prepare();
+        $Plugin = $self->Plugins->{$name};
+    }
+    $Plugin->run(@args);
+}
+
 1;
