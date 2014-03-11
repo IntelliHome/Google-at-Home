92f76392320f3755d29afd290adcb37fcad10274
mudler <mudler@dark-lab.net>
2014-03-12 00:43:16 +0100

    a plugin must have install/remove/update to handle database schema setup

--- lib/IH/Plugin/Base.pm
+++ lib/IH/Plugin/Base.pm
@@ -3,4 +3,22 @@ use Moose;
 has 'Config' => (is=>"rw");
 has 'Parser' => (is=>"rw");
 
+sub prepare {    # this is called on first load in the thread session if defined
+    my $self = shift;
+	$self->Parser->Output->debug("(Leaved as default) Preparing ".__PACKAGE__);
+}
+sub install { #Called on install
+	my $self=shift;
+	$self->Parser->Output->debug("(Leaved as default) Installing ".__PACKAGE__);
+}
+
+sub update { #Called on update
+	my $self=shift;
+	$self->Parser->Output->debug("(Leaved as default) Updating ".__PACKAGE__);
+}
+
+sub remove { #Called on remove
+	my $self=shift;
+	$self->Parser->Output->debug("(Leaved as default) Removing ".__PACKAGE__);
+}
 1;
