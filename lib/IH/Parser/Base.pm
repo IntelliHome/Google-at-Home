a5b2e15532b9cd9208f3fec432892941b8864ac6
mudler <mudler@dark-lab.net>
2014-03-12 00:44:03 +0100

    Running plugin safetly

--- lib/IH/Parser/Base.pm
+++ lib/IH/Parser/Base.pm
@@ -1,9 +1,10 @@
 package IH::Parser::Base;
 use Moose;
 use Module::Load;
-has 'Config' => ( is => "rw" );
-has 'Plugins' => ( is => "rw", default => sub{{}} );
-has 'Output' => (is=>"rw");
+has 'Config'  => ( is => "rw" );
+has 'Plugins' => ( is => "rw", default => sub { {} } );
+has 'Output'  => ( is => "rw" );
+
 sub detectTasks() {
 
 }
@@ -21,23 +22,30 @@ sub prepare() {
 }
 
 sub run_plugin() {
-    my $self = shift;
-    my $name = shift;
-    my $method =shift;
-    my @args = @_;
+    my $self   = shift;
+    my $name   = shift;
+    my $method = shift;
+    my @args   = @_;
     my $Plugin;
     if ( exists $self->Plugins->{$name} ) {
         $Plugin = $self->Plugins->{$name};
     }
     else {
         $Plugin = 'IH::Plugin::' . $name;
-        load $Plugin;
-        $self->Plugins->{$name}
-            = $Plugin->new( Config => $self->Config, Parser => $self );
-        $self->Plugins->{$name}->prepare() if $self->Plugins->{$name}->can("prepare") ;
-        $Plugin = $self->Plugins->{$name};
+        eval { load $Plugin; };
+        if ($@) {
+            $self->Output->error("Error loading plugin '$name' $@");
+            return 0;
+        }
+        else {
+            $self->Plugins->{$name}
+                = $Plugin->new( Config => $self->Config, Parser => $self );
+            $self->Plugins->{$name}->prepare()
+                if $self->Plugins->{$name}->can("prepare");
+            $Plugin = $self->Plugins->{$name};
+        }
     }
-    $Plugin->$method(@args);
+    return $Plugin->$method(@args) if ($Plugin->can($method));
 }
 
 1;
