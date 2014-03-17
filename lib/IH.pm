3475e363b7b8112e996f88b20b729a7fe0ec9243
mudler <mudler@dark-lab.net>
2014-03-17 15:00:15 +0100

    cleanup

--- lib/IH.pm
+++ lib/IH.pm
@@ -1,19 +1,5 @@
 package IH;
 
-our $VERSION="0.002";
+our $VERSION = "0.002";
 
-require IH::Pin::GPIO;
-require IH::Pin::Analogic;
-require Proc::Daemon;
-require IH::Interfaces::Terminal;
-require IH::Connector;
-require IH::Node;
-require IH::Workers::ListenAgent;
-require IH::Workers::Sox;
-require IH::Workers::Event;
-require IH::Workers::Monitor;
-require IH::Config;
-require IH::Workers::ListenNode;
-require AnyEvent;
-require IH::Workers::RemoteSynth;
 1;
