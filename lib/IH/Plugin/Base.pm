4972eb7f9d03edb02ce8b79a13372533561c7f5e
mudler <mudler@dark-lab.net>
2014-03-17 16:11:46 +0100

    cleanup

--- lib/IH/Plugin/Base.pm
+++ lib/IH/Plugin/Base.pm
@@ -1,26 +1,30 @@
 package IH::Plugin::Base;
 use Moose;
-has 'Config' => (is=>"rw");
-has 'Parser' => (is=>"rw");
+has 'Config' => ( is => "rw" );
+has 'Parser' => ( is => "rw" );
 
-sub prepare {    # this is called on first load in the thread session if defined
+sub prepare {  # this is called on first load in the thread session if defined
     my $self = shift;
-	$self->Parser->Output->debug("(Leaved as default) Preparing ".__PACKAGE__);
-}
-sub install { #Called on install
-	my $self=shift;
-	$self->Parser->Output->debug("(Leaved as default) Installing ".__PACKAGE__);
+    $self->Parser->Output->debug(
+        "(Leaved as default) Preparing " . __PACKAGE__ );
 }
 
-sub update { #Called on update
-	my $self=shift;
-	$self->Parser->Output->debug("(Leaved as default) Updating ".__PACKAGE__);
-	$self->remove();
-	$self->install();
+sub install {    #Called on install
+    my $self = shift;
+    $self->Parser->Output->debug(
+        "(Leaved as default) Installing " . __PACKAGE__ );
 }
 
-sub remove { #Called on remove
-	my $self=shift;
-	$self->Parser->Output->debug("(Leaved as default) Removing ".__PACKAGE__);
+sub update {     #Called on update
+    my $self = shift;
+    $self->Parser->Output->debug(
+        "(Leaved as default) Updating " . __PACKAGE__ );
+    return $self->remove() and $self->install();
+}
+
+sub remove {     #Called on remove
+    my $self = shift;
+    $self->Parser->Output->debug(
+        "(Leaved as default) Removing " . __PACKAGE__ );
 }
 1;
