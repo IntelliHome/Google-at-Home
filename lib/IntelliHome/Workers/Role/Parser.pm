ac25b42edfd0a354e92abd44a16f7deae587475b
mudler <mudler@dark-lab.net>
2014-04-03 00:40:57 +0200

    [enhancement] added IntelliHome::Workers::Role::Parser

---
+++ lib/IntelliHome/Workers/Role/Parser.pm
@@ -0,0 +1,18 @@
+package IntelliHome::Workers::Role::Parser;
+use Moo::Role;
+has 'Parser' => ( is => "rw", );
+
+sub BUILD {
+    my $self = shift;
+    my $Parser
+        = 'IntelliHome::Parser::'
+        . $self->Config->DBConfiguration->{'database_backend'};
+    load $Parser;
+    $Parser
+        = $Parser->new( Config => $self->Config, Output => $self->Output );
+    $self->Parser($Parser);
+    ##
+    $self->GSynth->Language( $self->Config->DBConfiguration->{'language'} );
+}
+
+1;
