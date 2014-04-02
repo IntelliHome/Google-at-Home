7a63044e8a117a9caa20617c3a2268547ef77229
mudler <mudler@dark-lab.net>
2014-04-02 23:49:33 +0200

    [enhancement] WiP in the listen agent worker

--- lib/IntelliHome/Workers/Agent/CommandProcess.pm
+++ lib/IntelliHome/Workers/Agent/CommandProcess.pm
@@ -1,5 +1,7 @@
 package IntelliHome::Workers::Agent::CommandProcess;
 use Moo;
+use IntelliHome::Utils qw(message_expand SEPARATOR);
+use Module::Load;
 
 has 'Output' =>
     ( is => "rw", default => sub { return new IntelliHome::Interfaces::Terminal } );
@@ -12,6 +14,19 @@ sub process {
         $command .= $_;
     }
     $self->Output->debug("I received - $command -");
+    my @args=message_expand($command);
+    my $type=shift @args;
+   	my $Pin="IntelliHome::Pin::".uc($type);
+   	my $id=shift @args;
+   	my $direction=shift @args;
+   	my $value=shift @args;
+   	my $Port=$Pin->new(
+   		Pin=> $id,
+   		Direction=>$direction
+   		);
+   	my $return=$Port->setValue($value);
+   	print $command $return;
+   	return $return;
 }
 
 1;
