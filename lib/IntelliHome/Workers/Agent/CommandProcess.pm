a94dc29df61c9c57374d8ca78ca83c69db625bd6
mudler <mudler@dark-lab.net>
2014-04-03 19:10:56 +0200

    [WiP] enhancement on Agent::CommandProcess

--- lib/IntelliHome/Workers/Agent/CommandProcess.pm
+++ lib/IntelliHome/Workers/Agent/CommandProcess.pm
@@ -3,8 +3,10 @@ use Moo;
 use IntelliHome::Utils qw(message_expand SEPARATOR);
 use Module::Load;
 
-has 'Output' =>
-    ( is => "rw", default => sub { return new IntelliHome::Interfaces::Terminal } );
+has 'Output' => (
+    is      => "rw",
+    default => sub { return new IntelliHome::Interfaces::Terminal }
+);
 
 sub process {
     my $self = shift;
@@ -14,19 +16,16 @@ sub process {
         $command .= $_;
     }
     $self->Output->debug("I received - $command -");
-    my @args=message_expand($command);
-    my $type=shift @args;
-   	my $Pin="IntelliHome::Pin::".uc($type);
-   	my $id=shift @args;
-   	my $direction=shift @args;
-   	my $value=shift @args;
-   	my $Port=$Pin->new(
-   		Pin=> $id,
-   		Direction=>$direction
-   		);
-   	my $return=$Port->setValue($value);
-   	print $command $return;
-   	return $return;
+    my @args = message_expand($command);
+    my $Pin  = "IntelliHome::Pin::" . uc( shift @args );
+    load $Pin;
+    my $Port = $Pin->new(
+        Pin       => shift @args,
+        Direction => shift @args
+    );
+    my $return = $Port->setValue( shift @args );
+    print $command $return;
+    return $return;
 }
 
 1;
