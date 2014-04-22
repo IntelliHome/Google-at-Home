d48c977c357a59d6ca517422dca01111757dd667
mudler <mudler@dark-lab.net>
2014-04-22 16:14:47 +0200

    [cleanup] Cleanup of Interfaces::Voices

--- lib/IntelliHome/Interfaces/Voice.pm
+++ lib/IntelliHome/Interfaces/Voice.pm
@@ -8,11 +8,13 @@ use IntelliHome::Connector;
 extends 'IntelliHome::Interfaces::Interface';
 
 #declare display to change: has this arguments (caller, method,@message)
-has 'TTS' => ( is => "rw", default => sub { return IntelliHome::Google::TTS->new } );
-has 'failback' =>
-    ( is => "rw", default => sub { return IntelliHome::Interfaces::Terminal->new } );
+has 'TTS' =>
+    ( is => "rw", default => sub { return IntelliHome::Google::TTS->new } );
+has 'failback' => (
+    is      => "rw",
+    default => sub { return IntelliHome::Interfaces::Terminal->new }
+);
 has 'Node'   => ( is => "rw" );
-has 'Config' => ( is => "rw" );
 
 sub BUILD {
     my $self = shift;
@@ -21,8 +23,7 @@ sub BUILD {
 }
 
 sub display {
-    my $self = shift;
-
+    my $self    = shift;
     my $caller  = shift;
     my $method  = shift;
     my @message = @_;
@@ -30,22 +31,20 @@ sub display {
     return $self->failback->debug( $caller, $method,
         "[FailBack Mode from TTS] ", @message )
         if $method eq "debug";
-
-    # print Dumper($self->Node);
     $self->TTS->text(@message);
-     my $errors=$self->TTS->tts();
+    my $errors = $self->TTS->tts();
+
     if (    $self->TTS->text()
         and -e $self->TTS->out
-        and defined( $self->Node() )
-        )
+        and defined( $self->Node() ) )
     {
         my $conn = IntelliHome::Connector->new( Node => $self->Node );
-      #  $self->failback->debug("sending raw audio");
         $conn->send_file( $self->TTS->out );
-
-        $self->failback->debug("There where ".$errors." errors in the generation of the response");
-        unlink($self->TTS->out);
-        unlink($_) for @{$self->TTS->responses};
+        $self->failback->debug( "There where "
+                . $errors
+                . " errors in the generation of the response" );
+        unlink( $self->TTS->out );
+        unlink($_) for @{ $self->TTS->responses };
     }
     else {
         $self->failback->debug( $caller, $method,
