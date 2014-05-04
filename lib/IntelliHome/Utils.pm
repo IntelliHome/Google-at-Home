34647b8d1ebdb2c5ba27b945cdc6bc8166def30a
mudler <mudler@dark-lab.net>
2014-05-04 19:32:40 +0200

    [enhancement] IntelliHome::Utils definition of daemonize()

--- lib/IntelliHome/Utils.pm
+++ lib/IntelliHome/Utils.pm
@@ -6,16 +6,38 @@ use base qw(Exporter);
 
 use constant SEPARATOR => ":";
 our @EXPORT_OK = qw(
-	message_expand SEPARATOR message_compact
-	);
-sub message_expand{
-	my $message=shift;
-	split(SEPARATOR,$message);
+    message_expand SEPARATOR message_compact daemonize
+);
+
+sub message_expand {
+    my $message = shift;
+    split( SEPARATOR, $message );
 }
 
-sub message_compact{
-	my @message=@_;
-	join(SEPARATOR,@message);
+sub message_compact {
+    my @message = @_;
+    join( SEPARATOR, @message );
+}
+
+sub daemonize {
+    use POSIX;
+    POSIX::setsid or die "setsid: $!";
+    my $pid = fork();
+    if ( $pid < 0 ) {
+        die "fork: $!";
+    }
+    elsif ($pid) {
+        exit 0;
+    }
+
+    #chdir "/";
+    umask 0;
+    foreach ( 0 .. ( POSIX::sysconf(&POSIX::_SC_OPEN_MAX) || 1024 ) ) {
+        POSIX::close $_;
+    }
+    open( STDIN,  "</dev/null" );
+    open( STDOUT, ">/dev/null" );
+    open( STDERR, ">&STDOUT" );
 }
 
 1;
