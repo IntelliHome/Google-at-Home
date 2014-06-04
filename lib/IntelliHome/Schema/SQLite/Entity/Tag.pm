329b8658f9e566ec8b22d4b1b6eb78f6d0a89b30
skullbocks <dgikille@gmail.com>
2014-06-04 21:47:10 +0200

    added Tag schema

--- lib/IntelliHome/Schema/SQLite/Entity/Tag.pm
+++ lib/IntelliHome/Schema/SQLite/Entity/Tag.pm
@@ -5,7 +5,7 @@ __PACKAGE__->table('tag');
 __PACKAGE__->add_columns(
 	'tagid' => { data_type=>'int', is_auto_increment=>1 },
 	'gpioid' => { data_type=>'int' }, 
-	'name', 
+	'tag', 
 	'description' => { is_nullable => 1 } );
 __PACKAGE__->set_primary_key('tagid');
 __PACKAGE__->belongs_to(gpio => 'IntelliHome::Schema::SQLite::Entity::GPIO', 'gpioid');
