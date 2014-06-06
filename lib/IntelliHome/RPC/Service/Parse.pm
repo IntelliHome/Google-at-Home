fb5fcfa989d2dabcec7560ef9b1dbb5e28ca7fbe
mudler <mudler@dark-lab.net>
2014-06-06 18:46:06 +0200

    [enhancement] removing useless block, using node in parser

--- lib/IntelliHome/RPC/Service/Parse.pm
+++ lib/IntelliHome/RPC/Service/Parse.pm
@@ -4,7 +4,6 @@ use Carp::Always;
 use Mojo::Base 'MojoX::JSON::RPC::Service';
 use feature 'say';
 use Data::Dumper;
-use IntelliHome::Utils qw(load_module);
 has 'IntelliHome';
 
 sub new {
@@ -16,13 +15,7 @@ sub new {
 
 sub parse {
     my ( $self, $tx, @params ) = @_;
-    my $node =
-        "IntelliHome::Schema::"
-      . $self->IntelliHome->Config->DBConfiguration->{'database_backend'}
-      . "::Node";
-    load_module($node);
-    my $Client = $node->new( Config => $self->IntelliHome->Config )
-      ->selectFromHost( $tx->remote_address, "node" );
+    my $Client = $self->IntelliHome->Parser->node->selectFromHost( $tx->remote_address, "node" );
     $self->IntelliHome->Parser->Node($Client);
     $self->IntelliHome->Parser->Output->Node($Client);
     $self->IntelliHome->Parser->parse(@params);
