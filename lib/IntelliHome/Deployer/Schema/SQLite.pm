4744b876d0c8e45dce7229422e43b276d00e565f
skullbocks <dgikille@gmail.com>
2014-06-26 16:28:56 +0200

    [SQLite #5] updated SQLite deployer to use Moo

--- lib/IntelliHome/Deployer/Schema/SQLite.pm
+++ lib/IntelliHome/Deployer/Schema/SQLite.pm
@@ -7,30 +7,36 @@ use 5.010;
 use DBIx::Class::DeploymentHandler;
 use lib '../../';
 use IntelliHome::Schema::SQLite::Schema;
-my $schema = IntelliHome::Schema::SQLite::Schema->connect('dbi:SQLite:/var/lib/intellihome/intellihome.db');
+use File::Path qw(make_path);
+use costant DBDIR => "/var/lib/intellihome/";
+use Moo;
 
-
-
-#my $schema = '...'; # wherever you get your schema from.
-my $deployment_handler_dir = '/var/lib/intellihome/db_upgrades';
-
-my $dh = DBIx::Class::DeploymentHandler->new(
-    {   schema           => $schema,
-        script_directory => $deployment_handler_dir,
-        databases        => 'SQLite',
-        force_overwrite  => 1,
-	    schema_version => $version
+has 'dee' => (
+    is      => "rw",
+    lazy    => 1,
+    default => sub {
+        make_path DBDIR unless -d DBDIR;
+        return DBIx::Class::DeploymentHandler->new(
+            {   schema => IntelliHome::Schema::SQLite::Schema->connect(
+                    'dbi:SQLite:' . DBDIR . 'intellihome.db'
+                ),
+                script_directory => DBDIR . 'db_upgrades',
+                databases        => 'SQLite',
+                force_overwrite  => 1,
+                schema_version   => $version
+            }
+        );
     }
 );
 
-
 sub prepare {
+    my $self         = shift;
     my $from_version = shift;
-    my $to_version = shift;
-    $dh->prepare_install;
+    my $to_version   = shift;
+    $self->dh->prepare_install;
 
     if ( defined $from_version && defined $to_version ) {
-        $dh->prepare_upgrade(
+        $self->dh->prepare_upgrade(
             {   from_version => $from_version,
                 to_version   => $to_version,
             }
@@ -39,24 +45,24 @@ sub prepare {
 }
 
 sub install {
+    my $self    = shift;
     my $version = shift;
-    $dh->prepare_install;
     if ( defined $version ) {
-        $dh->install({ schema_version => $version });
+        $self->dh->install( { schema_version => $version } );
     }
     else {
-        $dh->install;
+        $self->dh->install;
     }
 }
 
 sub upgrade {
-    $dh->upgrade;
+    return shift->upgrade;
 }
 
 sub database_version {
-    say $dh->database_version;
+    return shift->database_version;
 }
 
 sub schema_version {
-    say $dh->schema_version;
+    return shift->schema_version;
 }
