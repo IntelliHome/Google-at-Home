265fe9fedc05ef404a2b25ee26506707e3b4a62d
skullbocks <dgikiller@gmail.com>
2014-07-18 14:47:59 +0200

    [SQLite #5] added documentation for Command schema

--- lib/IntelliHome/Schema/SQLite/Schema/Result/Command.pm
+++ lib/IntelliHome/Schema/SQLite/Schema/Result/Command.pm
@@ -1,4 +1,49 @@
 package IntelliHome::Schema::SQLite::Schema::Result::Command;
+
+=head1 NAME
+
+IntelliHome::Schema::SQLite::Schema::Result::Command - DBIx::Class model that represent plugin exported functions
+
+=head1 DESCRIPTION
+
+This object is a model that represent plugin exported functions that have many triggers
+
+=head1 ATTRIBUTES
+
+=over
+
+=item commandid()
+
+command ID
+
+=item name()
+
+command name
+
+=item plugin()
+
+contains the plugin package name
+
+=item command()
+
+contains the plugin method to call
+
+=item triggers()
+
+all the triggers associated with that command
+
+=item commandgpio()
+
+all the gpio associated with that command
+
+=back
+
+=head1 SEE ALSO
+
+L<IntelliHome::Schema::SQLite::Schema::Result::Trigger>, L<IntelliHome::Schema::SQLite::Schema::Result::CommandGPIO>
+
+=cut
+
 use base qw/DBIx::Class::Core/;
  
 __PACKAGE__->table('command');
