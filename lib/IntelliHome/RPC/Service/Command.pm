0103ad7bc7fab2e154884f3325ddd12e05047f75
skullbocks <dgikiller@gmail.com>
2014-07-19 16:05:44 +0200

    [RPC #6] add documentation for IntelliHome::RPC::Service::Command

--- lib/IntelliHome/RPC/Service/Command.pm
+++ lib/IntelliHome/RPC/Service/Command.pm
@@ -1,5 +1,39 @@
 package IntelliHome::RPC::Service::Command;
 
+=head1 NAME
+
+IntelliHome::RPC::Service::Command - IntelliHome "Command" service for RPC server
+
+=head1 DESCRIPTION
+
+This object represent the "Command" service for the RPC server, it is available under C<rpcserver_ip:port/Command>
+
+=head1 METHODS
+
+IntelliHome::RPC::Service::Command inherits all methods from  L<IntelliHome::RPC::Service::Base> and implement the following new:
+
+=over
+
+=item gpio
+
+emit the "GPIO" event with the argument passed
+
+=item startup
+
+Starts the RPC server with the loaded services.
+
+=back
+
+=head1 ATTRIBUTES
+
+IntelliHome::RPC::Service::Command inherits all attributes from L<IntelliHome::RPC::Service::Base>
+
+=head1 SEE ALSO
+
+L<IntelliHome>, L<IntelliHome::Workers::Master::RPC> , L<MojoX::JSON::RPC::Service>
+
+=cut
+
 use Carp::Always;
 use Mojo::Base 'IntelliHome::RPC::Service::Base';
 use feature 'say';
