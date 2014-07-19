2a27d3ad0361ad5f2af79d0d0f93b4086f611d3b
skullbocks <dgikiller@gmail.com>
2014-07-19 16:05:27 +0200

    [RPC #6] update documentation for IntelliHome::RPC::Service::Base

--- lib/IntelliHome/RPC/Service/Base.pm
+++ lib/IntelliHome/RPC/Service/Base.pm
@@ -9,6 +9,16 @@ This object represent a base class for RPC Services for IntelliHome.
 It only ovverides the C<new> to inject C<'with_mojo_tx'=1> option by default.
 For more information on how services work, have a look at L<MojoX::JSON::RPC::Service>
 
+=head1 ATTRIBUTES
+
+=over
+
+=item IntelliHome
+
+contains an injected instance of L<IntelliHome::IntelliHomeRPC>
+
+=back
+
 =head1 SEE ALSO
 
 L<IntelliHome>, L<IntelliHome::Workers::Master::RPC>, L<MojoX::JSON::RPC::Service>
