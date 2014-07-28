1c255e69a23ddad87eaad72a30944b95bd92f7e7
skullbocks <dgikille@gmail.com>
2014-07-29 01:58:32 +0200

    [WebUI #7] Modified index draft

--- lib/IntelliHome/WebUI/Controller/Index.pm
+++ lib/IntelliHome/WebUI/Controller/Index.pm
@@ -1,25 +1,30 @@
 package IntelliHome::WebUI::Controller::Index;
-
-
 use Mojo::Base 'Mojolicious::Controller';
-
 use Mojo::Util qw(hmac_sha1_sum);
-
 use List::Util qw(min max);
-
-use Data::Dumper;
-
 use POSIX;
-
 use Encode;
-
+use IntelliHome::WebUI::Model::Tile;
 
 sub index {
-
 my $self = shift;
+my @tiles;
+my @rooms;
+#User login
+#if ( my $user = $self->session("username") ) {
+    #User is logged.
+#}
+# 
+# TODO: Add creation of tiles and rooms for every backend.
 
+@rooms = sort ( @rooms );
+
+$self->stash(
+    tiles      => \@tiles,
+    single        => 0,
+    rooms => \@rooms
+);
 $self->render("index");
-
 }
 
 
