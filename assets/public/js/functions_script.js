25c325407505463fe413dbf63adf857e547023cd
skullbocks <dgikille@gmail.com>
2014-08-07 12:35:10 +0200

    [WebUI #7] Scripts changes

---
+++ assets/public/js/functions_script.js
@@ -0,0 +1,44 @@
+(function (window, $, undefined) {
+
+	$(document).ready(function() {
+        var $command = $('.command');
+        var $tileDetail = $('.tile-detail');
+        var $nodeDelete = $('.node-delete');
+        var $gpioDelete = $('.gpio-delete');
+        var $gpioTags = $('.gpio-tags');
+        var $gpioPins = $('.gpio-pins');
+        var $gpioAdd = $('#add-gpio');
+        var $nodeAdd = $('#add-node');
+        $command.click(function() {
+            var $commandPost = $.post($(this).attr("data-post"),function( data ) {
+                    $( this ).toggleClass( "tile-status-off", data.result );
+                }, "json");
+        });
+        $tileDetail.click(function() {
+            window.location.href = this.attr("data-post");
+        });
+        $nodeDelete.click(function() {
+            var $nodeDeletePost = $.post("/delete-node/"+$(this).attr("id"),function( data ) {
+                    $("tr.node#"+$(this).attr("id")).remove();
+                }, "json");
+        });
+        $gpioDelete.click(function() {
+            var $gpioDeletePost = $.post("/delete-gpio/"+$(this).attr("id"),function( data ) {
+                    $("tr.gpio#"+$(this).attr("id")).remove();
+                }, "json");
+        });
+        $gpioTags.click(function() {
+            /* TODO: Open tags editable ( attr id as reference)*/
+        });
+        $gpioPins.click(function() {
+            /* TODO: Open pin editable ( attr id as reference) */
+        });
+        $gpioAdd.click(function() {
+            /* TODO: Open GPIO box to insert new gpio */
+        });
+        $nodeAdd.click(function() {
+            /* TODO: Open Node box to insert new gpio */
+        });
+        $('#fancyClock').tzineClock();
+	});
+})(window, jQuery);
--- assets/public/js/isotope_script.js
+++ assets/public/js/isotope_script.js
@@ -1,6 +1,6 @@
 (function (window, $, undefined) {
 
-	$(document).ready(function() {
+    $(document).ready(function() {
         var $room = $('.room');
         var $container = $('.iso,#iso').isotope({
             itemSelector: '.tile-item',
@@ -16,7 +16,26 @@
             }
         });
         $room.click(function() {
-            $container.isotope({ filter: $( this ).attr('id') });
+            $container.isotope({ filter: "."+$( this ).attr('id') });
         });
-	});
+        $('#searchString').keyup(function(e) {
+            clearTimeout($.data(this, 'timer'));
+            if (e.keyCode == 13){
+                var myFilterValue = search(true);
+                if (myFilterValue){
+                    $container.isotope({ filter: 'div.tile-item[data-tag="'+myFilterValue+'"]' });
+                }
+            } else {
+                $(this).data('timer', setTimeout(search, 500));
+            }
+        });
+        function search(force) {
+            var $existingString = $("#searchString").val();
+            if (!force && $existingString.length < 3) return; //wasn't enter, not > 2 char
+            $existingString = $existingString.toLowerCase();
+            var $name = $(this).find('.name').text();
+            var regex = new RegExp( $existingString, 'g' );
+            return name.match(regex);
+        }
+    });
 })(window, jQuery);
