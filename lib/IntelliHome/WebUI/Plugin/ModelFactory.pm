b084e2c9fbb4d62797796a152661256a2ffc149d
skullbocks <dgikille@gmail.com>
2014-08-09 00:12:04 +0200

    [WebUI #7] Replaced object in the backend, is safe in there

--- lib/IntelliHome/WebUI/Plugin/ModelFactory.pm
+++ lib/IntelliHome/WebUI/Plugin/ModelFactory.pm
@@ -9,23 +9,8 @@ sub register {
     $app->helper(
         build_tiles => sub {
             my @tiles;
-            push(
-                @tiles,
-                IntelliHome::WebUI::Model::Tile->new(
-                    title  => $_->tags->first(),
-                    id     => $_->gpioid,
-                    image  => 0,
-                    driver => $_->driver,
-                    status => $_->status,
-                    toggle => (
-                        ( split( /::/, ( $_->driver =~ /(.*)\=/ )[0] ) )[-1]
-                            eq "Mono"
-                        ) ? 1 : 0,
-                    gpio => $_->pin_id,
-                    room => $_->node->room->name,
-                    tag  => $_->tags->all()
-                )
-            ) for shift->rpc_call_blocking(@_);
+            push( @tiles, IntelliHome::WebUI::Model::Tile->new( %{$_} ) )
+                for shift->app->rpc_call_blocking(@_);
             return @tiles;
         }
     );
