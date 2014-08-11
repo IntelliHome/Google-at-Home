79b082a5d1302ba3b63f00f544e853dcb5bede57
skullbocks <dgikille@gmail.com>
2014-08-11 16:22:55 +0200

    [WebUI #7] Added fieds to rooms class

--- lib/IntelliHome/Schema/SQLite/Schema/Result/Room.pm
+++ lib/IntelliHome/Schema/SQLite/Schema/Result/Room.pm
@@ -41,7 +41,9 @@ __PACKAGE__->table('room');
 __PACKAGE__->add_columns(
 	'roomid' => { data_type=>'int', is_auto_increment=>1 }, 
 	'name', 
-	'location' => { is_nullable => 1} );
+	'location' => { is_nullable => 1},
+	'description' => { is_nullable => 1},
+	'notes' => { is_nullable => 1} );
 __PACKAGE__->set_primary_key('roomid');
 __PACKAGE__->has_many(nodes => 'IntelliHome::Schema::SQLite::Schema::Result::Node','roomid');
 
