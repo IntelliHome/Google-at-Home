37f301b1998f29bc92659489caa939f49d3314e8
skullbocks <dgikille@gmail.com>
2014-08-11 16:10:17 +0200

    [WebUI #7] Added functions to functions_script

--- assets/public/js/functions_script.js
+++ assets/public/js/functions_script.js
@@ -3,50 +3,44 @@
     $(document).ready(function() {
         var $command = $('.command');
         var $tileDetail = $('.tile-detail');
-        var $nodeDelete = $('.node-delete');
-        var $gpioDelete = $('.gpio-delete');
-        var $gpioTags = $('.gpio-tags');
-        var $gpioPins = $('.gpio-pins');
-        var $gpioAdd = $('#add-gpio');
-        var $nodeAdd = $('#add-node');
+        var $table = $('table');
+        var $gpioPopover = $('.gpio-tags,.gpio-pins');
+        var $addButton = $('#add-gpio,#add-node,#add-room,.add-tag');
         $command.click(function() {
             var $commandPost = $.post($(this).attr("data-post"), function(data) {
                 $(this).toggleClass("tile-status-off", data.result);
             }, "json");
         });
         $tileDetail.click(function() {
-            window.location.href = this.attr("data-post");
+            window.location.href = $(this).attr("data-post");
         });
-        $nodeDelete.click(function() {
-            var $nodeDeletePost = $.post("/delete-node/" + $(this).attr("id"), function(data) {
-                $("tr.node#" + $(this).attr("id")).remove();
+        $table.on('click', '.delete-' + $(this).attr("data-type"), function() {
+            $.post("/delete-" + $(this).attr("data-type") + "/" + $(this).parent().parent().attr("id"), function(data) {
+                $(this).parent().parent().remove();
             }, "json");
         });
-        $gpioDelete.click(function() {
-            var $gpioDeletePost = $.post("/delete-gpio/" + $(this).attr("id"), function(data) {
-                $("tr.gpio#" + $(this).attr("id")).remove();
-            }, "json");
+        $gpioPopover.popover({
+            html: true,
+            title: function() {
+                return $(this).parent().find('.popover-' + $(this).attr('data-poptype')).find('.head').html();
+            },
+            content: function() {
+                return $(this).parent().find('.popover-' + $(this).attr('data-poptype')).find('.content').html();
+            },
+            container: 'body',
+            placement: 'bottom'
         });
-        $gpioTags.click(function() {
-            /* TODO: Open tags editable ( attr id as reference)*/
-        });
-        $gpioPins.click(function() {
-            /* TODO: Open pin editable ( attr id as reference) */
-        });
-        $gpioAdd.click(function() {
-            /* TODO: Open GPIO box to insert new gpio */
-        });
-        $nodeAdd.click(function() {
-            $("#add-node-spinner").addClass('fa fa-spinner fa-spin');
-            $( "form#add-node-form" ).submit(function( event ) {
-                $.post( 'add-node/', $('form#add-node-form').serialize(), function(data) {
-                            $("#add-node-spinner").removeClass('fa fa-spinner fa-spin');
-                            $("#node-box").modal('hide').each(function() {
-                                    this.reset();
-                                });
-                        },
-                        'json' // JSON response
-                    );
+        $addButton.click(function() {
+            $("#" + $(this).attr('data-action') + "-spinner").addClass('fa fa-spinner fa-spin');
+            $("form#" + $(this).attr('data-action') + "-form").submit(function(event) {
+                $.post($(this).attr('data-action') + '/', $("form#" + $(this).attr('data-action') + "-form").serialize(), function(data) {
+                        $("#" + $(this).attr('data-action') + "-spinner").removeClass('fa fa-spinner fa-spin');
+                        $("#" + $(this).attr('data-action') + "-box").modal('hide').each(function() {
+                            this.reset();
+                        });
+                    },
+                    'json' // JSON response
+                );
                 event.preventDefault();
             });
         });
