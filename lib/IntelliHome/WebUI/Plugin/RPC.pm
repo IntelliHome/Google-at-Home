b2d143f03cc31b87b22c7507a8a4a715ac6e0781
skullbocks <dgikille@gmail.com>
2014-08-17 16:10:40 +0200

    [WebUI #7] Added docs for WebUI RPC plugin

--- lib/IntelliHome/WebUI/Plugin/RPC.pm
+++ lib/IntelliHome/WebUI/Plugin/RPC.pm
@@ -77,3 +77,55 @@ sub register {
 
 1;
 
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+IntelliHome::WebUI::Plugin::RPC - RPC plugin for mojolicious web application
+
+=head1 SYNOPSIS
+
+    my @Objs=rpc_call_blocking("Service",@args)
+    #or non blocking:
+    rpc_call("Service", sub { my ($res) = @_; ... },@params);
+
+=head1 DESCRIPTION
+
+This Mojolicious plugin allow to call the rpc server of IntelliHome
+
+=head1 METHODS
+
+=head2 rpc_call
+
+Async call to the RPC server.
+
+    rpc_call("Service", @params,
+        sub { my ($res) = @_; ... }
+        );
+
+
+=head2 rpc_call_blocking
+
+Blocking call to the RPC server
+
+    my @Objs=rpc_call_blocking("Service",@args)
+
+=head1 AUTHOR
+
+skullbocks E<lt>dgikiller@gmail.comtE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- skullbocks
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+L<IntelliHome::WebUI::Plugin::ModelFactory>, L<IntelliHome::IntelliHomeWebUI>
+
+=cut
